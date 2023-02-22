654021933ec71d2d486b8cad9adfcbf0251a3270
JuanMa Vázquez <94052849+Nostr4@users.noreply.github.com>
2023-02-22 09:41:10 +0100

    Update error2.php

--- HereAway/error2.php
+++ HereAway/error2.php
@@ -1,9 +1,9 @@
 <?php
+session_start();
 $title = "Cuenta inactiva";
 include ("bloques/doctype.php");
                 include ("bloques/header.php");
                 include ("bloques/nav.php");
-                session_start();
                 $_SESSION = array(); 
                 session_destroy();
                 ?>
